a9c389bf9cf21929d78707764a7a935df329c94c
DarioAlessioR <100212928+DarioAlessioR@users.noreply.github.com>
2022-06-05 13:47:29 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,126 @@
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
+# Project Name: Capstone Project 
+
+Description of the project:
+
+This is a page created to fulfill Microverse final requirements for Module 1. The project includes 1 homepage and 1 about page, according to template provided by Microverse.
+
+Instructions for the project specify that:
+
+- You should personalize the content of your page. Choose a topic that is different than the one in the original design.
+
+- You should follow these design guidelines, including:
+Colors.
+Typography: font face, size and weight.
+Layout: composition and space between elements.
+The pages should look almost identical to the original design. Small adjustments like text or image changes are acceptable.
+
+- You can use a CSS framework (for example Bootstrap) for styling, if you want to.
+
+- You should build only these 2 pages:
+
+The home page.
+
+The about page.
+
+Each of these pages should have versions for 2 different screen sizes:
+
+Mobile: up to 768px wide.
+
+Desktop: 768px or wider.
+
+Interactions
+
+- Links
+The home page should have a link in the menu to the about page.
+The logo in the header links to the home page.
+Mobile menu
+When the user clicks (or taps) the hamburger button on the header, the mobile menu appears over the page.
+There are no guidelines for the mobile menu in the docs, but you should implement it so it is consistent with the design (colors, typography, spacings, etc.).
+The mobile menu should have a close (X) button that closes the menu.
+Dynamic page
+
+- The section "Featured speakers" should be created dynamically in JavaScript.
+
+- You should use a JavaScript variable with the data about the speakers and use it when the page loads to create the HTML for this section dynamically.
+
+
+The project has been done following the template provided by Microverse in the following link:
+https://www.behance.net/gallery/29845175/CC-Global-Summit-2015
+
+## Built With
+
+-Major languages: HTML, CSS, Jvascript
+
+-Frameworks: Visual Studio Code
+
+-Technologies used: Git, GitHub, linters.
+
+## Live Demo (if available)
+
+No demo link available.
+
+
+## Getting Started
+
+No special instructions needed
+
+### Prerequisites
+
+No prerequisites
+
+### Setup
+
+No setup required
+
+### Install
+
+No install instructions required
+
+### Usage
+
+Usage only for educational purpouses
+
+### Run tests
+
+No test required
+
+### Deployment
+
+Deployment done with Github Pages.
+
+Link: https://darioalessior.github.io/my-capstone/
+
+### Video presentation of project
+
+Please, visit this link for a video preentation of the project: 
+https://www.loom.com/share/4dfe164e43f549a59974d6e1055f83a4
+
+## Authors
+
+👤 **Dario Alessio (@DarioAlessioR)**
+
+- GitHub: https://github.com/DarioAlessioR
+- Slack: https://microverse-students.slack.com/team/U039GCFRK9B
+- LinkedIn: https://www.linkedin.com/in/dario-alessio-3a3b7911b
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the issues page: (https://github.com/DarioAlessioR/my-capstone/issues).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Hat tip to anyone whose code was used and to reviewers that make possible constant improvement of this project.
+
+- Inspiration: Code with your brain, work with your heart.
+
+## 📝 License
+
+This project is [MIT](./MIT.md) licensed.
---
+++ index.js
@@ -0,0 +1,144 @@
+class Books {
+  constructor() {
+    this.id = '';
+    this.author = '';
+    this.title = '';
+    this.books = [];
+  }
+
+  addBook(id, author, title) {
+    this.id = id;
+    this.author = author;
+    this.title = title;
+    this.books.push({ id: this.id, author: this.author, title: this.title });
+  }
+
+  removeBook(id) {
+    this.books = this.books.filter((book) => book.id !== id);
+  }
+
+  getBooks() {
+    return this.books;
+  }
+
+  setBooks(par) {
+    this.books = par;
+  }
+}
+
+const Book = new Books();
+
+if (localStorage.getItem('books') !== null && localStorage.getItem('books') !== undefined) {
+  Book.setBooks(JSON.parse(localStorage.getItem('books')));
+}
+let books = Book.getBooks();
+
+const submitbtn = document.getElementById('submit');
+
+function Mybooks() {
+  if (books && books.length >= 0) {
+    const displyBooks = document.querySelector('.bookslist');
+    displyBooks.innerHTML = '';
+    books.forEach((book) => {
+      const list = document.createElement('li');
+      const author = document.createElement('p');
+      const btn = document.createElement('button');
+      btn.innerHTML = 'Remove';
+      author.innerHTML = `"${book.title}" by ${book.author}`;
+      btn.setAttribute('id', book.id);
+      btn.setAttribute('class', 'remove-btn');
+      btn.setAttribute('onclick', `removeme(${book.id});`);
+      list.appendChild(author);
+      list.appendChild(btn);
+      displyBooks.appendChild(list);
+    });
+  }
+}
+
+function addbook(title, author) {
+  if (title.length < 2 && author.length < 1) {
+    const message = document.getElementById('message');
+    message.innerHTML = 'please, fill all input fields';
+  } else {
+    Book.addBook(books && books.length > 0 ? books[books.length - 1].id + 1 : 1, author, title);
+    books = Book.getBooks();
+    localStorage.setItem('books', JSON.stringify(books));
+    Mybooks();
+  }
+}
+
+submitbtn.addEventListener('click', () => {
+  const title = document.getElementById('input-title');
+  const author = document.getElementById('input-author');
+  const titleIpnut = title.value;
+  const authorIpnut = author.value;
+  addbook(titleIpnut, authorIpnut);
+  author.value = '';
+  title.value = '';
+});
+
+function removeme(id) {
+  Book.removeBook(Number(id));
+  books = Book.getBooks();
+  localStorage.setItem('books', JSON.stringify(books));
+  Mybooks();
+}
+
+const Links = document.querySelectorAll('.links');
+Links.forEach((link) => {
+  link.addEventListener('click', () => {
+    const sections = document.querySelectorAll('section');
+    sections.forEach((sec) => {
+      sec.classList.add('hide');
+      if (sec.classList.contains('showElement')) {
+        sec.classList.remove('showElement');
+      }
+    });
+    const section = document.querySelector(`section.${link.classList[1]}`);
+    section.classList.add('showElement');
+  });
+});
+
+/*
+
+const aa = document.querySelector('.add-section');
+const ll = document.querySelector('.list-section');
+const cc = document.querySelector('.contact-section');
+const lissec = document.querySelector('.books', '.list-section')
+const lisadd = document.querySelector('.add', '.add-section')
+const liscon = document.querySelector('.contact', '.contact-section')
+
+
+ll.addEventListener('click', () => {
+  lissec.classList.add('showElement');
+  lissec.classList.remove('hide');
+  lisadd.classList.add('hide');
+  lisadd.classList.remove('showElement');
+  liscon.classList.add('hide');
+  liscon.classList.remove('showElement');
+});
+  
+aa.addEventListener('click', () => {
+  lissec.classList.add('hide');
+  lissec.classList.remove('showElement');
+  lisadd.classList.add('showElement');
+  lisadd.classList.remove('hide');
+  liscon.classList.add('hide');
+  liscon.classList.remove('showElement');
+});
+
+cc.addEventListener('click', () => {
+  lissec.classList.add('hide');
+  lissec.classList.remove('showElement');
+  lisadd.classList.add('hide');
+  lisadd.classList.remove('showElement');
+  liscon.classList.add('showElement');
+  liscon.classList.remove('hide');
+});
+*/
+
+const dateTime = document.querySelector('.current-date');
+dateTime.innerHTML = `${new Date().toLocaleDateString()},  ${new Date().toLocaleTimeString()}`;
+
+removeme();
+Mybooks();
